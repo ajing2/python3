146f9394a2836ca2453f3805061996b8d499290e
ling <974644081@qq.com>
2017-12-23 11:02:11 +0800

    python3讲课实例

---
+++ InstallPacharm/demon1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/23 10:31
+# @Author  : lingxiangxiang
+# @File    : demon1.py
+import sys
+
+name = input("please input your name: ")
+print("hello {0}".format(name))
+
+
+# python2  input  输入的是整型       raw_input   输入的是字符串
+# python3   只有input  输入的是字符串
---
+++ demon2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/23 10:53
+# @Author  : lingxiangxiang
+# @File    : demon2.py
+print("hello world")
+for i in range(1, 10):
+    print(i)
